3633f85b2a7b12ea0cd2dc715fee2a7592a981a7
quancq <quancq.it@gmail.com>
2018-08-12 17:10:26 +0700

    Add file contain all hyper parameters

---
+++ hyper_parameters.py
@@ -0,0 +1,11 @@
+# File contains all hyper parameters
+
+MAX_WORD_LENGTH = 20
+MIN_WORD_LENGTH = 2
+NUM_LABELS = 22
+MIN_OCCURRENCES_TOKEN = 3
+
+SCORING = ["f1_macro", "f1_micro", "accuracy"]
+CV = 3
+RANDOM_STATE = 7
+VOCAB_PATH = "./Vocabulary/vocab_17012.csv"
